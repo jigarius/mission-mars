a3867caf4f99b36996ae231713d6266073796281
Jigar Mehta <jigarius@gmail.com>
2021-01-04 22:41:03 -0500

    Create Command

---
+++ lib/command.rb
@@ -0,0 +1,10 @@
+# typed: strict
+# frozen_string_literal: true
+
+class Command < T::Enum
+  enums do
+    M = new
+    L = new
+    R = new
+  end
+end
---
+++ spec/command_spec.rb
@@ -0,0 +1,15 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'command'
+
+describe Command do
+  it 'contains all possible values' do
+    expected = [
+      Command::M,
+      Command::L,
+      Command::R
+    ]
+    expect(Command.values).to eq(expected)
+  end
+end
