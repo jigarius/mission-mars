07b3edfff45e7e5ba56feab7d429fea652328820
Jigar Mehta <jigarius@gmail.com>
2021-01-05 11:43:25 -0500

    Handle all rovers in #handle_rover_entries

--- lib/controller.rb
+++ lib/controller.rb
@@ -19,27 +19,26 @@ class Controller
   def execute
     introduce
 
-    input = Input::Parser.new.parse_stdin
-    @martian_plateau = Region::SimpleRectangularRegion.new(input.limit)
+    @input = Input::Parser.new.parse_stdin
+    @martian_plateau = Region::SimpleRectangularRegion.new(@input.limit)
 
-    input.rover_entries.map do |re|
-      rover = handle_rover_entry(re)
-      "#{rover.coordinates.x} #{rover.coordinates.y} #{rover.direction}"
-    end
+    handle_rover_entries
   end
 
   private
 
-  sig { params(rover_entry: Input::RoverEntry).returns(Rover) }
-  def handle_rover_entry(rover_entry)
-    rover = Rover.new(
-      coordinates: rover_entry.coordinates,
-      direction: rover_entry.direction,
-      region: @martian_plateau
-    )
+  sig { returns(T::Array[String]) }
+  def handle_rover_entries
+    @input.rover_entries.map do |rover_entry|
+      rover = Rover.new(
+        coordinates: rover_entry.coordinates,
+        direction: rover_entry.direction,
+        region: @martian_plateau
+      )
 
-    rover_entry.commands.each { |c| rover.execute(c) }
+      rover_entry.commands.each { |c| rover.execute(c) }
 
-    rover
+      "#{rover.coordinates.x} #{rover.coordinates.y} #{rover.direction}"
+    end
   end
 end
